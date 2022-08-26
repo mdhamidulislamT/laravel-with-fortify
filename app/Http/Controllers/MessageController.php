563db5188683ec4db64d71331fee95eaa2cff474
mdhamidulislamT <hamidlive6@gmail.com>
2022-08-26 12:18:34 +0600

    message model added

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -4,18 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Events\Message;
+use App\Models\Message as ModelsMessage;
 use Illuminate\Support\Facades\Auth;
 
 class MessageController extends Controller
 {
     public function message(Request $request)
     {
+        $data = $request->all();
         $userType = Auth::user()->id;
-        $username = Auth::user()->name;
-        $message = $request->message;
-        event(new Message($userType, $username, $message));
+        $data['user_id'] = $userType;
+        $result = ModelsMessage::create($data);
+        $success = '';
+        
+        if ($result) {
+            $username = Auth::user()->name;
+            $message = $request->message;
+            event(new Message($userType, $username, $message));
+            $success = ['data' => "Message Sent Successfully!"];
+        } else {
+            $success = ['data' => "Message Not Sent!"];
+        }
 
-        response()->json(['data'=> "Message Sent Successfully!"]);
+        response()->json($success);
     }
-
 }
